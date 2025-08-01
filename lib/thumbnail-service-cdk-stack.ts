dd49b7d60c5451775cc0fc77f33e7674c4894bb6
yishai <yishai5555@gmail.com>
2025-08-01 16:27:49 +0300

    01-handler-and-s3-bucket

--- lib/thumbnail-service-cdk-stack.ts
+++ lib/thumbnail-service-cdk-stack.ts
@@ -1,16 +1,29 @@
 import * as cdk from 'aws-cdk-lib';
+import { RemovalPolicy } from 'aws-cdk-lib';
+import { Function } from 'aws-cdk-lib/aws-lambda';
 import { Construct } from 'constructs';
-// import * as sqs from 'aws-cdk-lib/aws-sqs';
+import { join } from 'path';
+import * as s3 from 'aws-cdk-lib/aws-s3';
 
 export class ThumbnailServiceCdkStack extends cdk.Stack {
   constructor(scope: Construct, id: string, props?: cdk.StackProps) {
     super(scope, id, props);
+    const handler = new Function(this, 'handler-function-resizeImg', {
+      runtime: cdk.aws_lambda.Runtime.PYTHON_3_9,
+      timeout: cdk.Duration.seconds(20),
+      handler: 'app.s3_thumbnail_generator',
+      code: cdk.aws_lambda.Code.fromAsset(join(__dirname, '../lambda')),
+      environment: {
+        REGION_NAME: 'us-east-1',
+        THUMBNAIL_SIZE: '128',
+      },
+    });
 
-    // The code that defines your stack goes here
+    const s3Bucket = new s3.Bucket(this, 'photo-bucket', {
+      removalPolicy: RemovalPolicy.DESTROY,
+      autoDeleteObjects: true,
+    });
 
-    // example resource
-    // const queue = new sqs.Queue(this, 'ThumbnailServiceCdkQueue', {
-    //   visibilityTimeout: cdk.Duration.seconds(300)
-    // });
+    s3Bucket.grantReadWrite(handler);
   }
 }
