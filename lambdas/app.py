75049e026072a6832ef0d24704110bfe52698128
yishai <yishai5555@gmail.com>
2025-08-01 17:41:18 +0300

    05-fix-typo-in-function-name

--- lambdas/app.py
+++ lambdas/app.py
@@ -1,4 +1,5 @@
-def s3s3_thumbnail_generator_ths3_thumbnail_generator(event, context):
+def s3_thumbnail_generator(event, context):
     print("should shrink image to thumbnail size")
 
     return "This was triggerd by an s3 Bucket event. (should shrink image)"
+
