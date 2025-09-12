d1409df4e8fd0818dea69d81fa429089f1a193d8
yishai <yishai5555@gmail.com>
2025-09-12 17:16:00 +0300

    08-fixes

--- lambdas/app.py
+++ lambdas/app.py
@@ -12,7 +12,6 @@ s3 = boto3.client('s3')
 size = int(os.environ.get('THUMBNAIL_SIZE'))
 # size = int(os.environ.get('THUMBNAIL_SIZE', 128))
 
-
 def s3_thumbnail_generator(event, context):
     print("Event:::", event)
 
@@ -20,23 +19,22 @@ def s3_thumbnail_generator(event, context):
     key = event['Records'][0]['s3']['object']['key']
     img_size = event['Records'][0]['s3']['object']['size']
 
-    if (not key.endswith('_thumbnail.png')):
+    if not key.endswith('_thumbnail.png'):
         # get the image from s3
-        image = s3.get_object(Bucket=bucket_name, Key=key)
+        img = get_s3_image(bucket_name, key)
 
         # resize the image
-        thumbnail = image_to_thumbnail(image)
+        thumbnail = image_to_thumbnail(img)
 
         # get the new filename
         thumbnail_key = new_filename(key)
 
         # upload the file
-        url = upload_to_s3(bucket_name, thumbnail, thumbnail_key, img_size)
+        url = upload_to_s3(bucket_name, thumbnail_key, thumbnail, img_size)
 
         print("Image:::", url)
 
         return url
-
 
 def get_s3_image(bucket, key):
     response = s3.get_object(Bucket=bucket, Key=key)
@@ -47,8 +45,10 @@ def get_s3_image(bucket, key):
     return img
 
 
-def image_to_thumbnail(image):
-    return ImageOps.fit(image(size, size), Image.Analytics)
+def image_to_thumbnail(img):
+    # Resize and crop to square
+    return ImageOps.fit(img, (size, size), Image.ANTIALIAS)
+
 
 # def new_filename(key):
 #     key_split = key.split('.')
@@ -63,8 +63,7 @@ def new_filename(key):
 
 def upload_to_s3(bucket, key, image, img_size):
     out_thumbnail = BytesIO()
-
-    out_thumbnail.save(out_thumbnail, 'PNG')
+    image.save(out_thumbnail, 'PNG')
     out_thumbnail.seek(0)
 
     response = s3.put_object(
