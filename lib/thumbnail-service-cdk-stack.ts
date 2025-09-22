4a762b21102b4fe429056577fb541ab6d7453b89
yishai <yishai5555@gmail.com>
2025-09-22 15:11:03 +0300

    09-add-layer-pil-fix

--- lib/thumbnail-service-cdk-stack.ts
+++ lib/thumbnail-service-cdk-stack.ts
@@ -18,8 +18,9 @@ export class ThumbnailServiceCdkStack extends cdk.Stack {
       layers: [
         LayerVersion.fromLayerVersionArn(
           this,
-          'PIL',  
-          'arn:aws:lambda:us-east-1:770693421928:layer:Klayers-p39-Pillow:15'
+          'PIL',
+          // 'arn:aws:lambda:us-east-1:770693421928:layer:Klayers-p39-Pillow:15'
+          'arn:aws:lambda:us-east-1:770693421928:layer:Klayers-p39-pillow:1'
         ),
       ],
       environment: {
