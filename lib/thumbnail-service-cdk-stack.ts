b359742b5b3849b999568784ecc2f9daf7bdaa64
yishai <yishai5555@gmail.com>
2025-09-26 15:54:38 +0300

    11-build-with-docker

--- lib/thumbnail-service-cdk-stack.ts
+++ lib/thumbnail-service-cdk-stack.ts
@@ -1,7 +1,7 @@
 import * as cdk from 'aws-cdk-lib';
 import { RemovalPolicy } from 'aws-cdk-lib';
 import { Effect, PolicyStatement } from 'aws-cdk-lib/aws-iam';
-import { Function, LayerVersion } from 'aws-cdk-lib/aws-lambda';
+import { Function, LayerVersion, Architecture } from 'aws-cdk-lib/aws-lambda';
 import { Construct } from 'constructs';
 import { join } from 'path';
 import * as s3 from 'aws-cdk-lib/aws-s3';
@@ -13,10 +13,24 @@ export class ThumbnailServiceCdkStack extends cdk.Stack {
 
     const pillowLayer = new LayerVersion(this, 'PillowLayer', {
       code: cdk.aws_lambda.Code.fromAsset(
-        join(__dirname, '../lambda-layer-pillow/pillow-layer.zip')
+        join(__dirname, '../lambda-layer-pillow'),
+        {
+          bundling: {
+            image: cdk.DockerImage.fromRegistry(
+              'public.ecr.aws/lambda/python:3.12'
+            ),
+            command: [
+              'bash',
+              '-lc',
+              // Install Pillow into the required layer directory structure at /asset-output/python
+              'pip install --no-cache-dir Pillow -t /asset-output/python',
+            ],
+          },
+        }
       ),
       compatibleRuntimes: [cdk.aws_lambda.Runtime.PYTHON_3_12],
       description: 'Layer with Pillow for image processing',
+      compatibleArchitectures: [Architecture.X86_64],
     });
 
     const handler = new Function(this, 'handler-function-resizeImg', {
@@ -25,24 +39,25 @@ export class ThumbnailServiceCdkStack extends cdk.Stack {
       handler: 'app.s3_thumbnail_generator',
       code: cdk.aws_lambda.Code.fromAsset(join(__dirname, '../lambdas')),
       layers: [pillowLayer],
+      architecture: Architecture.X86_64,
       environment: {
         REGION_NAME: 'us-east-1',
         THUMBNAIL_SIZE: '128',
       },
     });
 
-          // LayerVersion.fromLayerVersionArn(
-      //   this,
-      //   'PIL',
-      //   // 'arn:aws:lambda:us-east-1:770693421928:layer:Klayers-p39-Pillow:15'
-      //   // 'arn:aws:lambda:us-east-1:770693421928:layer:Klayers-p39-pillow:1'
-      //   // 'arn:aws:lambda:us-east-1:770693421928:layer:Klayers-p311-google-cloud-bigquery:22'
-      //     //  'arn:aws:lambda:us-east-1:770693421928:layer:Klayers-p311-pillow:1'
-      //     'arn:aws:lambda:us-east-1:770693421928:layer:Klayers-p312-pillow:1'
+    // LayerVersion.fromLayerVersionArn(
+    //   this,
+    //   'PIL',
+    //   // 'arn:aws:lambda:us-east-1:770693421928:layer:Klayers-p39-Pillow:15'
+    //   // 'arn:aws:lambda:us-east-1:770693421928:layer:Klayers-p39-pillow:1'
+    //   // 'arn:aws:lambda:us-east-1:770693421928:layer:Klayers-p311-google-cloud-bigquery:22'
+    //     //  'arn:aws:lambda:us-east-1:770693421928:layer:Klayers-p311-pillow:1'
+    //     'arn:aws:lambda:us-east-1:770693421928:layer:Klayers-p312-pillow:1'
 
-      //   //
-      // ),
-      // ],
+    //   //
+    // ),
+    // ],
 
     const s3Bucket = new s3.Bucket(this, 'photo-bucket', {
       removalPolicy: RemovalPolicy.DESTROY,
