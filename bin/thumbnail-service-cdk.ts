56a63912a02cabeb044859b94ff3edec249cefcd
yishai <yishai5555@gmail.com>
2025-10-01 15:39:36 +0300

    16-rename-fix-2

--- bin/thumbnail-service-cdk.ts
+++ bin/thumbnail-service-cdk.ts
@@ -3,4 +3,9 @@ import * as cdk from 'aws-cdk-lib';
 import { ThumbnailServiceCdkStack } from '../lib/thumbnail-service-cdk-stack';
 
 const app = new cdk.App();
-new ThumbnailServiceCdkStack(app, 'ThumbnailServiceCdkStackUniqueName', {});
+const stack = new ThumbnailServiceCdkStack(app, 'ThumbnailServiceCdkStack', {});
+
+stack.renameLogicalId(
+  'ThumbnailServiceCdkStack',           // old id
+  'ThumbnailServiceCdkStackUniqueName'  // new id
+);
